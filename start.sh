100065aacbf03925e6d24c9a19cb85618d64c14e
Lita <lita@knowtions.com>
2021-07-15 15:46:13 +0800

    set up cron to start server with script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bash
+set -e
+set -o pipefail
+
+# start a detached tmux session if not having one
+tmux has-session -t server
+if [ $? -ne 0 ]; then
+    tmux new-session -d -t server
+fi
+
+# start the server in tmux session if server is not running
+ps ux | grep java | grep -v grep
+if [ $? -ne 0 ]; then
+    tmux send-key \
+        "java -Xmx${MAX_MEM} -jar server.jar" \
+        C-m # <C-m> is return key
+fi
